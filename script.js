353c54070eb742616ee4564d7e8f9b56bc3b8792
Oleg Brazhnichenko <Oleg.brazhnichenko@gmail.com>
2017-06-10 01:38:05 +0300

    add upgrade

--- script.js
+++ script.js
@@ -12,14 +12,28 @@ var templates = {
         +   'price:{{price}}<br>'
         +   'level:{{level}} <br>'
         +   '<input type="button" value="sell" onclick="sell({{id}})">'
-        +   '<input type="button" value="update" onclick="update({{id}})">'
+        +   '<input type="button" value="update" onclick="upgrade({{id}})">'
         +'</li>'
 };
 
 window.onload = function(){
     load_shop();
     show_user_info(load_user_info());
+    load_prices();
 };
+
+function load_prices(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange= function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                localStorage.setItem("prices",httpRequest.responseText);
+            }
+        }
+    };
+    httpRequest.open('GET', "./storage/prices.json");
+    httpRequest.send();
+}
 
 function load_shop() {
     var httpRequest = new XMLHttpRequest();
@@ -86,7 +100,7 @@ function buy(item_name){
         }else{
             item["id"] = new Date().getTime();
             item["level"] = 0;
-            item["price"] = item["price"] - Math.round(item["price"]/100*20);
+            item["price"] = Math.round(item["price"]/100*80);
             user_info.weapons.push(item);
             user_info.balance -= item["price"];
         }
@@ -111,4 +125,40 @@ function sell(item_id){
     }
     show_user_info(user_info);
     localStorage.setItem("user_info", JSON.stringify(user_info));
+}
+
+function upgrade(item_id){
+    var user_info = load_user_info();
+    var item = user_info.weapons.filter(function( obj ) {
+        return obj.id == item_id;
+    })[0];
+
+    if (user_info.upgrade_kits >= 1){
+        user_info.upgrade_kits--;
+        if (Math.floor(Math.random()*100) < 63 || item["level"] < 3){
+            var default_price = JSON.parse(localStorage.getItem("shop")).filter(function( obj ) {
+                return obj.name == item["name"];
+            })[0].price;
+            var prices = JSON.parse(localStorage.getItem("prices"));
+            $.each(user_info.weapons, function() {
+                if (this.id == item["id"]) {
+                    this.level++;
+
+                    this.price = default_price/100*80 + 1050*this.level + prices[this.level];
+
+                    return false;
+                }
+
+                return true;
+            });
+        }else{
+            user_info.weapons = $.grep(user_info.weapons, function(weapon) {
+                return weapon.id !== item["id"];
+            });
+        }
+        show_user_info(user_info);
+        localStorage.setItem("user_info", JSON.stringify(user_info));
+    }else{
+        alert("Not enough upgrade kits. Come on, buy one more :)");
+    }
 }
